2a72e33cc10f66b0690740bfa42574c1bbc8aa66
Eddie Kao <eddie@5xcampus.com>
2023-10-13 17:00:13 +0800

    add simple http server

--- vivvi.js
+++ vivvi.js
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import http from "http"
+import color from "picocolors"
+
+const { PORT_HTTP, PROJECT_NAME } = process.env
+
+http
+  .createServer((req, res) => {
+    res.end("hi")
+  })
+  .listen(PORT_HTTP)
+
+console.log(
+  `${color.red(PROJECT_NAME)} server ON! ${color.green(
+    `http://localhost:${PORT_HTTP}`
+  )}`
+)
