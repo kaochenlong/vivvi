93fa277174f41c3a52477b20ca4690e5041c5d94
Eddie Kao <eddie@5xcampus.com>
2023-10-15 14:45:11 +0800

    add watcher

--- vivvi/client.js
+++ vivvi/client.js
@@ -1,8 +1,39 @@
+// deno-lint-ignore-file
 const ws = new WebSocket("ws://localhost:9527")
 
 ws.addEventListener("open", ({ target: socket }) => {
   socket.addEventListener("message", ({ data }) => {
-    console.log(data)
+    const result = JSON.parse(data)
+
+    switch (result.type) {
+      case "message":
+        console.log(result.content)
+        break
+      case "change":
+        const { file } = result
+
+        if (file.endsWith(".css")) {
+          fetch(file)
+            .then((data) => data.text())
+            .then((content) => {
+              const el = document.querySelector("style#hot")
+              if (el) {
+                el.textContent = content
+              } else {
+                const el = document.createElement("style")
+                el.id = "hot"
+                el.textContent = content
+                document.querySelector("head").appendChild(el)
+              }
+            })
+            .catch((err) => console.log(err))
+          // 換
+        } else {
+          window.location.reload()
+        }
+
+        break
+    }
   })
 
   socket.addEventListener("close", () => {
--- vivvi/server.js
+++ vivvi/server.js
@@ -1,10 +1,13 @@
 import http from "http"
 import color from "picocolors"
 import connect from "connect"
+import chokidar from "chokidar"
 import { WebSocketServer } from "ws"
 import { indexHTMLMiddleware, replaceImportMiddleware } from "./middlewares"
+import { getRelativePath } from "./utils.js"
 
 const { PORT_HTTP, PROJECT_NAME, PORT_WS } = process.env
+const WATCH_LIST = ["index.html", "src/*.js", "src/*.css"]
 
 const createWSServer = () => {
   const server = new WebSocketServer({ port: PORT_WS })
@@ -12,7 +15,19 @@ const createWSServer = () => {
   server.on("connection", (ws) => {
     console.log(color.green("WebSocket Connected"))
 
-    ws.send(`${PROJECT_NAME} Connected!`)
+    ws.send(
+      JSON.stringify({ type: "message", content: `${PROJECT_NAME} Connected!` })
+    )
+
+    // watcher
+    const watcher = chokidar.watch(WATCH_LIST)
+    watcher.on("change", (file) => {
+      const msgObj = {
+        type: "change",
+        file: getRelativePath(file),
+      }
+      ws.send(JSON.stringify(msgObj))
+    })
 
     ws.on("message", (data) => {
       console.log("Received: %s", data)
